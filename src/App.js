9a4e036d748b00365c474a021b0af2357494f8c5
Huang-Ming Chang <huang.m.chang@gmail.com>
2018-11-09 17:26:39 +0100

    basic UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import styled from 'react-emotion';
+import {
+  Button,
+  Card as AntCard,
+  List,
+  Skeleton as AntSkeleton,
+  Switch,
+  Input as AntInput
+} from 'antd';
+
+const Container = styled('div')({
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: '#f7f7f7'
+});
+
+const Card = styled(AntCard)({
+  boxShadow: '-1px 2px 24px -4px rgba(117,117,117,0.72)',
+  minWidth: 600
+});
+
+const Skeleton = styled(AntSkeleton)({
+  paddingLeft: 20
+});
+
+const Input = styled(AntInput)({
+  border: 'none',
+  boxShadow: 'none !important'
+});
 
 class App extends Component {
   render() {
+    const list = [
+      { loading: true, name: 'foo', done: true },
+      { loading: false, name: 'bar', done: false }
+    ];
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-            <Button>Click</Button>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Card
+          title={<Input placeholder="What you want to do next?" size="large" />}
+        >
+          <List
+            className="demo-loadmore-list"
+            itemLayout="horizontal"
+            dataSource={list}
+            renderItem={item => (
+              <List.Item actions={[<a>delete</a>]}>
+                <List.Item.Meta
+                  avatar={<Switch checked={item.done} />}
+                  title={item.name}
+                />
+              </List.Item>
+            )}
+          />
+        </Card>
+      </Container>
     );
   }
 }
