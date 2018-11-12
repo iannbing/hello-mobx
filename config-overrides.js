aba8b3ee6b1bfa2fd7a9af6b8d289ba7fb8d5c8f
Huang-Ming Chang <huang.m.chang@gmail.com>
2018-11-12 15:01:14 +0100

    use .env and .env.local

--- config-overrides.js
+++ config-overrides.js
@@ -7,10 +7,12 @@ const {
   addWebpackAlias
 } = require('customize-cra');
 
+console.log(process.env.REACT_APP_BUNDLE_VISUALIZE);
+
 module.exports = override(
   addDecoratorsLegacy(),
   disableEsLint(),
-  process.env.BUNDLE_VISUALIZE === 'true' && addBundleVisualizer(),
+  process.env.REACT_APP_BUNDLE_VISUALIZE === 'true' && addBundleVisualizer(),
   addWebpackAlias({ '@ant-design/icons': 'purched-antd-icons' }), // only include used Icons
   fixBabelImports('antd', {
     libraryDirectory: 'lib',
