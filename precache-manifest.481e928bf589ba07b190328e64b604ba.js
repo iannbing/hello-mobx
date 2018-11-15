e428fa33e1fbbba6452e4343568729f4100a4d25
Huang-Ming Chang <huang.m.chang@gmail.com>
2018-11-15 14:03:48 +0100

    Updates

---
+++ precache-manifest.481e928bf589ba07b190328e64b604ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1bf082a996bb8ee241b6",
+    "url": "/static/js/main.1bf082a9.chunk.js"
+  },
+  {
+    "revision": "d55724a04dc87cb36173",
+    "url": "/static/js/1.d55724a0.chunk.js"
+  },
+  {
+    "revision": "d55724a04dc87cb36173",
+    "url": "/static/css/1.6fa10709.chunk.css"
+  },
+  {
+    "revision": "bc8ff2d2f186aa33f44c27b01d26f951",
+    "url": "/index.html"
+  }
+];
